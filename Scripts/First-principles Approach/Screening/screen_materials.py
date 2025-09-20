f2598684ec63f47e8abe3e21136ab4e4df1b0ea8
Amit Chaudhari <amit_321@hotmail.co.uk>
2025-09-20 20:02:56 +0200

    Update screen_materials.py

--- Scripts/First-principles Approach/Screening/screen_materials.py
+++ Scripts/First-principles Approach/Screening/screen_materials.py
@@ -1,6 +1,6 @@
 #This script is from the ChemRxiv pre-print "Machine learning generalised DFT+U projectors in a numerical atom-centred orbital framework" at the DOI 10.26434/chemrxiv-2025-332z0 DOI: 10.26434/chemrxiv-2025-332z0
-#This script is used in Section 3.3.4, to perform a linear search of the first-principle cost function (J_FP) for all materials specified in References.txt
-#The linear search of J_FP uses empirical correlations for DFT+U-predicted O 2p orbital occupancies computed using hierarchical symbolic regression, as defined in HI-SISSO.py
+#This script is used in Section 3.3.4, to perform a linear search of the Hubbard parameter space for all materials in References.txt 
+#For each screened combination of Hubbard parameters, DFT+U-predicted orbital occupancies are predicted using empirical correlations from hierarchical symbolic regression, as defined in HI-SISSO.py
 
 import csv
 import subprocess
