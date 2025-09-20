a3945473d53457ccf1d1371316a92bd44ce0f4b8
Amit Chaudhari <amit_321@hotmail.co.uk>
2025-09-20 19:51:13 +0200

    Add Overview Cost_function.py

--- Scripts/First-principles Approach/Screening/Cost_function.py
+++ Scripts/First-principles Approach/Screening/Cost_function.py
@@ -1,6 +1,6 @@
 #This script is from the ChemRxiv pre-print "Machine learning generalised DFT+U projectors in a numerical atom-centred orbital framework" at the DOI 10.26434/chemrxiv-2025-332z0 DOI: 10.26434/chemrxiv-2025-332z0
-#This script is used in Section 3.3.4, to perform a linear search of the first-principle cost function (J_FP) for all materials specified in References.txt
-#The linear search of J_FP uses empirical correlations for O 2p orbital occupancies computed using hierarchical symbolic regression, as defined in HI-SISSO.py
+#This script is used in Section 3.3.4, to evaluate and screen the first-principle cost function (J_FP) for all materials specified in References.txt and save as new {material}_with_JFP.txt file
+#The linear search of J_FP uses empirical correlations for DFT+U-predicted O 2p orbital occupancies computed using hierarchical symbolic regression, as defined in HI-SISSO.py
 
 import pandas as pd
 import numpy as np
